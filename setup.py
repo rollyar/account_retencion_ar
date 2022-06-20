566b83f5d6b8e65397f927ed32bbbababf98438f
Rolando Arnaudo <rolando.arnaudo@gmail.com>
2022-06-19 21:11:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,10 +52,10 @@ if minor_version % 2:
 else:
     branch = series
 
-download_url = 'https://github.com/tryton-ar/account_retencion_ar/tree/%s' % branch
+download_url = 'https://github.com/rollyar/account_retencion_ar/tree/%s' % branch
 
 LINKS = {
-    'trytonar_account_voucher_ar': ('git+https://github.com/tryton-ar/'
+    'trytonar_account_voucher_ar': ('git+https://github.com/rollyar/'
         'account_voucher_ar.git@%s#egg=trytonar_account_voucher_ar-%s' %
         (branch, series)),
     }
